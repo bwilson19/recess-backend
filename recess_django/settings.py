3012f161d6c68f6dab9afaa5f7149fc606589fd3
bwilson19 <brendan.wilson19@gmail.com>
2020-03-06 14:19:49 -0800

    superuser issue

--- recess_django/settings.py
+++ recess_django/settings.py
@@ -78,13 +78,10 @@ WSGI_APPLICATION = 'recess_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
