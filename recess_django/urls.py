c06765be4def6f0f1e93e5cdefc606b85c73e4f3
bwilson19 <brendan.wilson19@gmail.com>
2020-03-09 10:36:41 -0700

    building auth

--- recess_django/urls.py
+++ recess_django/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('recess.urls')),
+    path('token-auth/', obtain_jwt_token)
 ]
