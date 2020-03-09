6025f0736d4f489bccd94f84944759b45e480789
bwilson19 <brendan.wilson19@gmail.com>
2020-03-09 14:51:39 -0700

    adding fbook auth

--- recess_django/urls.py
+++ recess_django/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('recess.urls')),
-    path('token-auth/', obtain_jwt_token)
+    path('token-auth/', obtain_jwt_token),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
