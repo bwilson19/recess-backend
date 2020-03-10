ab134f8dedbf4452c9c91b1a487335cae77cd49c
bwilson19 <brendan.wilson19@gmail.com>
2020-03-10 11:05:02 -0700

    changing models

--- recess/models.py
+++ recess/models.py
@@ -9,7 +9,6 @@ class League(models.Model):
     city = models.CharField(max_length=100)
     sport = models.CharField(max_length=100)
     rules = models.TextField()
-    image_url = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +22,7 @@ class Game(models.Model):
     zipcode = models.CharField(max_length=5)
     date = models.DateField()
     info = models.TextField()
-    image_url = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
     league = models.ForeignKey(
         League, on_delete=models.CASCADE, related_name='games')
 
